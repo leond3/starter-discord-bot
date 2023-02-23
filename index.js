086b066f33af6a54ee97f0b91a6781afb7c50d89
leond3 <59891470+leond3@users.noreply.github.com>
2023-02-23 22:01:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,89 +1,83 @@
-
-// const { clientId, guildId, token, publicKey } = require('./config.json');
-require('dotenv').config()
-const APPLICATION_ID = process.env.APPLICATION_ID 
-const TOKEN = process.env.TOKEN 
-const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set'
-const GUILD_ID = process.env.GUILD_ID 
+require('dotenv').config();
+const APPLICATION_ID = process.env.APPLICATION_ID ;
+const TOKEN = process.env.TOKEN ;
+const PUBLIC_KEY = process.env.PUBLIC_KEY || 'not set';
+const GUILD_ID = process.env.GUILD_ID ;
 
 
-const axios = require('axios')
+const axios = require('axios');
 const express = require('express');
 const { InteractionType, InteractionResponseType, verifyKeyMiddleware } = require('discord-interactions');
 
-
 const app = express();
-// app.use(bodyParser.json());
 
 const discord_api = axios.create({
-  baseURL: 'https://discord.com/api/',
-  timeout: 3000,
-  headers: {
-	"Access-Control-Allow-Origin": "*",
-	"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
-	"Access-Control-Allow-Headers": "Authorization",
-	"Authorization": `Bot ${TOKEN}`
-  }
+	baseURL: 'https://discord.com/api/',
+	timeout: 3000,
+	headers: {
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+		"Access-Control-Allow-Headers": "Authorization",
+		"Authorization": `Bot ${TOKEN}`
+	}
 });
 
 
 
 
 app.post('/interactions', verifyKeyMiddleware(PUBLIC_KEY), async (req, res) => {
-  const interaction = req.body;
+	const interaction = req.body;
 
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    console.log(interaction.data.name)
-    if(interaction.data.name == 'color'){
-      return res.send({
-        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        data: {
-          content: `This command is not yet functional!`,
-        },
-      });
-    }
-  }
+	if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+		console.log(interaction.data.name);
+		if (interaction.data.name === 'color') {
+			return res.send({
+				type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+				data: {
+					content: `This command is not yet functional!`,
+				},
+			});
+		}
+	}
 });
 
 
 
 app.get('/register_commands', async (req,res) =>{
-  let slash_commands = [
-    {
-      "name": "color",
-      "description": "Allows you to change your role color",
-      "options": [
-	      {
-		      name: 'hex',
-		      description: 'hex color code',
-		      required: true
-	      }
-      ]
-    }
-  ]
-  try
-  {
-    // api docs - https://discord.com/developers/docs/interactions/application-commands#create-global-application-command
-    let discord_response = await discord_api.put(
-      `/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
-      slash_commands
-    )
-    console.log(discord_response.data)
-    return res.send('commands have been registered')
-  }catch(e){
-    console.error(e.code)
-    console.error(e.response?.data)
-    return res.send(`${e.code} error from discord`)
-  }
-})
+	let slash_commands = [
+		{
+			"name": "color",
+			"description": "Allows you to change your role color",
+			"options": [
+				{
+					name: 'hex',
+					description: 'hex color code',
+					required: true
+				}
+			]
+		}
+	];
+	try {
+		let discord_response = await discord_api.put(
+			`/applications/${APPLICATION_ID}/guilds/${GUILD_ID}/commands`,
+			slash_commands
+		)
+		console.log(discord_response.data);
+		return res.send('commands have been registered');
+	} catch (e) {
+		console.error(e.code);
+		console.error(e.response?.data);
+		return res.send(`${e.code} error from discord`);
+	}
+});
 
 
-app.get('/', async (req,res) =>{
+app.get('/', async (req,res) => {
   return res.send('Follow documentation ')
-})
+});
 
 
 app.listen(8999, () => {
 
-})
+});
 
